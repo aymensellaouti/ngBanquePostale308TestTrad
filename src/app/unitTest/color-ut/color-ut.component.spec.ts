1741a42b51bd9019dcee45f7fc652f59d14a428c
aymen2782 <aymen.sellaouti@gmail.com>
2023-09-01 16:26:07 +0100

    correction exo test

--- src/app/unitTest/color-ut/color-ut.component.spec.ts
+++ src/app/unitTest/color-ut/color-ut.component.spec.ts
@@ -1,10 +1,13 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { ColorUtComponent } from "./color-ut.component";
+import { DebugElement } from "@angular/core";
+import { By } from "@angular/platform-browser";
 
 fdescribe("ColorUtComponent", () => {
   let component: ColorUtComponent;
   let fixture: ComponentFixture<ColorUtComponent>;
+  let el: DebugElement;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ColorUtComponent],
@@ -12,10 +15,25 @@ fdescribe("ColorUtComponent", () => {
 
     fixture = TestBed.createComponent(ColorUtComponent);
     component = fixture.componentInstance;
+    el = fixture.debugElement;
     fixture.detectChanges();
   });
 
   it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  /* Le premier test: vérifier que la div est rouge */
+  it("should have red div", () => {
+    const para = el.query(By.css(`[data-testid=para]`));
+    expect(para.nativeElement.style["background-color"]).toBe("red");
+  });
+  /* Le deuxième test: vérifier que apres le click ca devienne jaune  */
+
+  it("should have yellow div after clicking the p", () => {
+    const para = el.query(By.css(`[data-testid=para]`));
+    para.nativeElement.click();
+    fixture.detectChanges();
+    expect(para.nativeElement.style["background-color"]).toBe("yellow");
   });
 });
--- src/app/unitTest/mathService/math.service.spec.ts
+++ src/app/unitTest/mathService/math.service.spec.ts
@@ -1,7 +1,29 @@
-describe("MathService", () => {
+import { LoggerService } from "../../services/logger.service";
+import { MathService } from "./math.service";
+
+/* let loggerSpy: jasmine.Spy; */
+let mathService: MathService;
+let loggerService: LoggerService;
+fdescribe("MathService", () => {
+  beforeEach(() => {
+    let loggerSpy = jasmine.createSpyObj("LoggerService", ["logger"]);
+    mathService = new MathService(loggerSpy);
+  });
   it("should work", () => {
     // Todo : Test if the service is well instanciated
-
     expect(1).toBe(1);
+  });
+  /* Il doit bien additionner */
+  it("should add two numbers", () => {
+    /* Act */
+    const result = mathService.add(1, 2);
+    /* Assert */
+    expect(result).toBe(3);
+  });
+  it("should substract two numbers", () => {
+    /* Act */
+    const result = mathService.substract(1, 2);
+    /* Assert */
+    expect(result).toBe(-1);
   });
 });
